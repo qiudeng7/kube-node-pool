496437c6f829b47caca965f4fd23fc869e192c21
qiudeng7 <2143975743@qq.com>
2026-01-31 21:48:40 +0800

    perf: 优化k3s join为从国内下载

--- server/kube/assets/k3s_init.sh
+++ server/kube/assets/k3s_init.sh
@@ -3,9 +3,8 @@
 # 脚本第一个参数为 k3s token
 export K3S_TOKEN="${1:-123456}"
 
+# 国内二进制下载源
 export INSTALL_K3S_MIRROR="cn"
 
 # 国内源安装k3s
 curl -sfL https://rancher-mirror.rancher.cn/k3s/k3s-install.sh | sh -s - server --cluster-init --system-default-registry "registry.cn-hangzhou.aliyuncs.com"
-
-
--- server/kube/assets/k3s_join_master.sh
+++ server/kube/assets/k3s_join_master.sh
@@ -6,4 +6,7 @@ export K3S_TOKEN="${1:-123456}"
 # 脚本第二个参数为 k3s master 地址
 export MASTER_IP="${2}"
 
+# 国内二进制下载源
+export INSTALL_K3S_MIRROR="cn"
+
 curl -sfL https://rancher-mirror.rancher.cn/k3s/k3s-install.sh | K3S_TOKEN="${K3S_TOKEN}" sh -s - server --server https://${MASTER_IP}:6443 --system-default-registry "registry.cn-hangzhou.aliyuncs.com"
